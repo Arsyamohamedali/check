600f70b43c2d43d70b77191dbb09df48994e647e
Arsyamohamedali <70328502+Arsyamohamedali@users.noreply.github.com>
2021-03-06 19:05:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from apps import home, app1,app2 # import your app modules here
 app = MultiApp()
 st.sidebar.title('Image Analysis')
 # Add all your application here
-app.add_app("MedAI", home.app)
+app.add_app("Home", home.app)
 app.add_app("Haemorrhage Detection", app1.app)
 app.add_app("Pneumonia Detection",app2.app)
 
