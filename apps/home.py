be4aad15409f3a9b242e070e29e5744eac5d462a
Arsyamohamedali <70328502+Arsyamohamedali@users.noreply.github.com>
2021-03-14 11:59:26 +0530

    Update home.py

--- apps/home.py
+++ apps/home.py
@@ -61,7 +61,7 @@ def app():
         font-weight:bold;
     }
     </style>
-""")
+""", unsafe_allow_html=True)
     st.markdown('<p class="bigg-font">Need Medical Assistance??</p>', unsafe_allow_html=True)
 
     st.write("***Here's the solution***")
@@ -76,7 +76,7 @@ def app():
         font-weight:bold;
     }
     </style>
-""", unsafe_allow_html=True)
+""")
     st.button('<p class="bigg-font">Need Medical Assistance??</p>')
 
     #st.markdown("### Sample Data")
