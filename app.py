b150f188c5615b118b8dd1be963ca84c511e11f8
Arsyamohamedali <70328502+Arsyamohamedali@users.noreply.github.com>
2021-03-15 14:41:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,6 +6,16 @@ from PIL import Image
 hi=Image.open('radiographer.png')
 st.set_page_config(page_title='MedAI',page_icon=hi)
 
+st.markdown(
+    '''
+    <style>
+    .html {
+    background-image: url("https://img.freepik.com/free-vector/white-elegant-texture-wallpaper_23-2148421854.jpg?size=626&ext=jpg&ga=GA1.2.145878890.1611360000");
+    background-size: cover;
+    }
+    </style>
+    ''', 
+    unsafe_allow_html=True)
 
 app = MultiApp()
 st.sidebar.title('Image Analysis')
