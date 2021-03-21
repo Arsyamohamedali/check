1f9cde6b5f94de47e68498240fc5ce3a3bfd7d98
Arsyamohamedali <70328502+Arsyamohamedali@users.noreply.github.com>
2021-03-21 11:29:23 +0530

    Update app1.py

--- apps/app1.py
+++ apps/app1.py
@@ -14,7 +14,7 @@ def app():
 	page_bg_img = '''
 	<style>
 	body {
-	background-image: url("https://img.freepik.com/free-vector/white-elegant-texture-wallpaper_23-2148421854.jpg?size=626&ext=jpg&ga=GA1.2.145878890.1611360000");
+	background-color: #DADED4;
 	background-size: cover;
 	}
 	</style>
