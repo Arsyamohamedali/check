c8c07d2f283a28f223f8c0edda07f550bca47cf7
Arsyamohamedali <70328502+Arsyamohamedali@users.noreply.github.com>
2021-03-22 13:00:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,17 +6,6 @@ from PIL import Image
 hi=Image.open('radiographer.png')
 st.set_page_config(page_title='MedAI',page_icon=hi)
 
-st.markdown(
-    '''
-    <style>
-    .html {
-    background-image: url("https://img.rawpixel.com/s3fs-private/rawpixel_images/website_content/rm21-background-tong-058.jpg?w=800&dpr=1&fit=default&crop=default&q=65&vib=3&con=3&usm=15&bg=F4F4F3&ixlib=js-2.2.1&s=710a6fed5b1923da8d5f95191839ef8a");
-    background-size: cover;
-    }
-    </style>
-    ''', 
-    unsafe_allow_html=True)
-
 app = MultiApp()
 st.sidebar.title('Image Analysis')
 # Add all your application here
